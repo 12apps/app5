3c92cde1ac751501127c295a2dcfd2ea80adcd57
CodingHouse <sb@codinghouse.co>
2014-12-16 15:17:45 -0800

    Basic notification

--- background.js
+++ background.js
@@ -5,4 +5,9 @@ chrome.app.runtime.onLaunched.addListener(function() {
       'height': 500
     }
   });
+  setTimeout(function() {
+    chrome.notifications.create("main", { type: "basic", title: "Ready", iconUrl: "calculator-16.png",  message: "It's Ready" }, function() {
+      console.log(this);
+    });
+  }, 4000);
 });
