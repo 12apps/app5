73a5c33f3c25984ad76c9581708aa0fee27b3f90
CodingHouse <sb@codinghouse.co>
2014-12-16 17:02:50 -0800

    Fix the id of notification

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,10 +1,13 @@
 Firebase.enableLogging(true);
 var q = new Firebase('https://codinghouse.firebaseio.com/queue');
 
+var i = 0;
 q.on("value", function(snapshot) {
-  chrome.notifications.create("main", { type: "basic", title: "CodingHouse", iconUrl: "/calculator-16.png",  message: snapshot.val().message }, function() {
+  console.log("H", snapshot, snapshot.val());
+  chrome.notifications.create("main-" + i , { type: "basic", title: "CodingHouse", iconUrl: "/calculator-16.png",  message: snapshot.val().message }, function() {
     console.log(this);
   });
+  i = i +1;
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
